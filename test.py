09eab6508866b0a4078f3e4baf98c7cf8c012724
ng-truong <ngtruong@tamu.edu>
2019-02-21 22:44:22 -0600

    checking requests

--- test.py
+++ test.py
@@ -11,7 +11,19 @@ regex = r"(.*) - - \[(.*)\] \"(.*?) (.*?\..*?) (HTTP\/\d\.\d)\" (\d*) (\d*)"
 
 # answer questions:
 #     1. How many total requests were made in the time period represented in the log?
+#       726,736
 #     2. How many requests were made on each day? per week? per month?
+#       example: 
+total = 0
+
+with open('Oct1994.txt') as f:
+        for line in f:
+                finded = line.find('24/Oct/1994')
+                if finded != -1 and finded != 0:
+                        total += 1
+
+print(total)
+#       run for every date to get value
 #     3. What percentage of the requests were not successful (any 4xx status code)?
 #     4. What percentage of the requests were redirected elsewhere (any 3xx codes)?
 #     5. What was the most-requested file?
