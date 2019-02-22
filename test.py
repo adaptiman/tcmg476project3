3426d21223500e9359a36c1cbedb399bce61790c
ng-truong <ngtruong@tamu.edu>
2019-02-21 22:53:32 -0600

    count

--- test.py
+++ test.py
@@ -18,12 +18,34 @@ total = 0
 
 with open('Oct1994.txt') as f:
         for line in f:
-                finded = line.find('24/Oct/1994')
+                finded = line.find('Oct/1994')
                 if finded != -1 and finded != 0:
                         total += 1
 
 print(total)
 #       run for every date to get value
+# Oct/1994: 11168
+        # 24: 705
+        # 25: 1194
+        # 26: 1680
+        # 27: 1889
+        # 28: 1817
+        # 29: 898
+        # 30: 902
+        # 31: 2083
+        # 23/10/94-30/10/94: 9085
+# Nov/1994
+# Dec/1994
+# Jan/1995
+# Feb/1995
+# Mar/1995
+# Apr/1995
+# May/1995
+# Jun/1995
+# Jul/1995
+# Aug/1995
+# Sep/1995
+# Oct/1995
 #     3. What percentage of the requests were not successful (any 4xx status code)?
 #     4. What percentage of the requests were redirected elsewhere (any 3xx codes)?
 #     5. What was the most-requested file?
